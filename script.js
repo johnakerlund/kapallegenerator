c801c80641efa5beca96ce057f857d784ea72d5b
johnakerlund <john.ake@hotmail.com>
2019-08-16 14:49:10 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+function generator(){
+    var first = ["Anal","Bög","Bajs","Kuk","Snopp","Kiss","Mini","Anus","Fitt","Bullemi","Blöj","CP","Samba","Disco","Downs","Runk","Dvärg","Euro","Hiv","Mjölk","Downs","Damp","Mes","Stor","Mini"];
+    var second =["Bögen","Röven","Mackan","Kissarn","Runkaren","Fittan","Bullen","Bajsarn","Sovarn","Lover","Hivet","Aidset"];
+
+    var double = Math.floor(Math.random()*8);
+   
+    var textBox = document.getElementById('TEXTBOX_ID').value;
+
+    console.log(double);
+    if(double == 1){
+
+        var name = first[Math.floor(Math.random()* first.length)]+ first[Math.floor(Math.random()* first.length)] + "-"+second[Math.floor(Math.random()* second.length)];  
+    }
+    else if(double == 2){
+        var name = first[Math.floor(Math.random()* first.length)] + "-"+ textBox
+     
+    }
+    else if (double ==7 ){
+
+        var name = first[Math.floor(Math.random()* first.length)]+ first[Math.floor(Math.random()* first.length)] + "-"+ textBox
+    }
+    else{
+    var name = first[Math.floor(Math.random()* first.length)] + "-"+second[Math.floor(Math.random()* second.length)];
+    }
+    
+
+    if (document.getElementById("name")) {
+		document.getElementById("placeholder").removeChild(document.getElementById("name"));
+	}
+    var element = document.createElement("div");
+	element.setAttribute("id", "name");
+	element.appendChild(document.createTextNode(name));
+	document.getElementById("placeholder").appendChild(element);
+
+  
+}
